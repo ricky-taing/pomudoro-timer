8519202fbc67df18c49ac09acb2d3b0c9ce28438
ricky-taing <48187498+ricky-taing@users.noreply.github.com>
2024-03-22 22:46:00 -0400

    used gemini pro to generate text-to-text content

--- gemini-api.js
+++ gemini-api.js
@@ -1,4 +1,16 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+const fs = require('fs');
 require('dotenv').config();
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+
+async function run() {
+    const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+    const prompt = 'Write a story about magic backpack.';
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+}
+run();
